bbb723492b14a474f6d7a6fb0b1841142c5de3e1
arpitbhuker <arpitkumarbhuker@gmail.com>
2025-10-07 22:34:49 +0530

    feat: Add Google Analytics 4 (GA4) tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { useGoogleAnalytics } from './useGoogleAnalytics'; 
+
+useGoogleAnalytics("G-VE5FFS1TKS");
+
 createRoot(document.getElementById("root")!).render(<App />);
